2b64fc1ffa14ebccc2feb719542fd9ceedd3cd0d
Andrés Barraza Valencia <andres_bava@hotmail.com>
2020-11-26 21:34:07 -0700

    Create POOP2P2#3_Barraza_Andres.cpp

---
+++ POOP2P2#3_Barraza_Andres.cpp
@@ -0,0 +1,27 @@
+/* ejercicio #3*/
+/*Cree un programa donde use un archivo de cabecera donde esté la clase Trabajador. 
+Se debe ingresar mediante el teclado el sueldo mensual, sueldo por hora extra y la cantidad de 
+horas extras trabajadas para calcular el dinero que obtuvo el trabajador en un año.*/
+
+#include <iostream>
+#include "cabecera.h"
+
+sing namespace std;
+
+int main(){
+    int mes, horasExt, numHorasExt;
+    cout << "ingrese los datos de un trabajador\n"
+         << endl;
+    cout << "ingrese el sueldo mensual:";
+    cin >> mes;
+    cout << endl;
+    cout << "ingrese el sueldo por hora extra:";
+    cin >> horasExt;
+    cout << endl;
+    cout << "ingrese la cantidad de horas extras trabajadas:";
+    cin >> numHorasExt;
+    cout << endl;
+    Trabajador empleado(mes, horasExt, numHorasExt);
+    cout << "El sueldo anual del trabajador es: $" << obrero.sueldo() << endl;
+    return 0;
+}
